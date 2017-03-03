f0630ddc943f3ce664adacd92c9815451dde07fc
ldesgoui <ldesgoui@gmail.com>
2017-03-03 12:10:05 +0100

    new version, next is complete rewrite. if you use this, edit webhooks file by removing anything before the last 2 parts of the url (basically delete until webhooks/) and change your cron to POST at /webhooks/check

--- app.py
+++ app.py
@@ -13,34 +13,47 @@ import time
 
 CONTENT = "New TF2 Blog Post"
 TF2_RSS = "http://www.teamfortress.com/rss.xml"
+BASE = "https://discordapp.com/api/webhooks/"
 COLOR = 11751957
 
 os.environ["TZ"] = "Europe/Paris"
 time.tzset()
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/', methods=["POST", "GET"])
+@application.route('/', methods=["POST", "GET"])
 def index():
+
     failed = False
+
     if request.method == "POST":
-        webhook = request.form["webhook"]
+        webhook = "/".join(request.form["webhook"].split("/")[-2:])
         failed = True
+
         try:
-            if not webhook.startswith("https://discordapp.com/api/webhooks/"):
-                raise Exception
-            rss = feedparser.parse(TF2_RSS)
-            response = requests.post(webhook, params={'wait': True},
-                    json={"content": "Webhook successfully registered! Here's an example",
-                          "embeds": [format_embed(random.choice(rss['entries']))]})
+            response = requests.get(BASE + webhook)
             if response.status_code != 200:
                 raise Exception
+
+            rss = feedparser.parse(TF2_RSS)
+
+            response = requests.post(BASE + webhook, params={ 'wait': True }, json={
+                    "content": "Webhook successfully registered! Here's an example",
+                    "embeds": [format_embed(random.choice(rss['entries']))]
+            })
+            if response.status_code != 200:
+                raise Exception
+
             with open("webhooks", "r") as f:
                 webhooks = set(f.readlines())
+
             webhooks.add(webhook + "\n")
+
             with open("webhooks", "w") as f:
                 f.write("".join(webhooks))
+
             failed = False
+
         except:
             pass
 
@@ -66,15 +79,17 @@ def format_embed(entry):
     }
 
 
-def rss_check():
+
+@application.route("/check", methods=["POST"])
+def check():
+    out = []
+
     with open("last_update") as last_update_file:
         last_update = float(last_update_file.read())
 
     rss = feedparser.parse(TF2_RSS, modified=time.localtime(last_update))
-
     if rss.status == 304:
-        exit()
-
+        return "nothing new"
 
     payload = {
             "content": CONTENT,
@@ -89,24 +104,23 @@ def rss_check():
     with open("last_update", "w") as last_update:
         last_update.write(str(latest_update))
 
+    valid_webhooks = set()
+
     with open("webhooks") as webhooks:
         for webhook in webhooks:
-            response = requests.post(webhook[:-1], json=payload)
+            response = requests.get(BASE + webhook[:-1], headers={'content-type': 'application/json'})
+            if response.status_code != 200:
+                out.append("GET({r.status_code}: {r.reason}) {r.text} {r.url}".format(r=response))
+                continue
+
+            response = requests.post(BASE + webhook[:-1], json=payload)
             if response.status_code >= 300:
-                print("Webhook failed ({r.status_code}: {r.reason}) {r.text} {r.url}".format(r=response))
-            time.sleep(1)
+                out.append("POST({r.status_code}: {r.reason}) {r.text} {r.url}".format(r=response))
+                continue
 
-    latest_update = time.mktime(rss["updated_parsed"])
+            valid_webhooks.add(webhook)
 
-    with open("last_update", "w") as last_update:
-        last_update.write(str(latest_update))
+    with open("webhooks", "w") as webhooks:
+        webhooks.write("".join(valid_webhooks))
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="TF2 Blog Discord Webhook Blarg")
-    parser.add_argument("--check", help="Check RSS for new entries instead of running web app", action='store_true')
-    args = parser.parse_args()
-    if args.check:
-        rss_check()
-    else:
-        app.run(debug=True)
+    return "\n".join(out)
